781c0ade84121c2c01b3a4d300440d33a3c3a00c
vkaruri <vkaruri@princeton.edu>
2017-12-19 21:39:15 -0500

    Added game control buttons

--- gameoflife/src/index.js
+++ gameoflife/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { ButtonToolbar, MenuItem, DropdownButton } from 'react-bootstrap';
 
 class Box extends React.Component {
     selectBox = () => {
@@ -47,6 +48,49 @@ class Grid extends React.Component {
 				{rowsArr}
 			</div>
 		)
+	}
+}
+
+class Buttons extends React.Component {
+	handleSelect = (evt) => {
+			this.props.gridSize(evt);
+	}
+	
+	render() {
+		return (
+			<div className="center">
+			    <ButtonToolbar>
+					<button className="btn btn-default" onClick={this.props.playButton}>
+						Play
+					</button>
+					<button className="btn btn-default" onClick={this.props.pauseButton}>
+						Pause
+					</button>
+					<button className="btn btn-default" onClick={this.props.clear}>
+						Clear
+					</button>
+					<button className="btn btn-default" onClick={this.props.slow}>
+						Slow
+					</button>
+					<button className="btn btn-default" onClick={this.props.fast}>
+						Fast
+					</button>
+					<button className="btn btn-default" onClick={this.props.seed}>
+						Seed
+					</button>
+
+					<DropdownButton
+						title="Grid Size"
+						id="size-menu"
+						onSelect={this.handleSelect}
+					>
+						<MenuItem eventKey="1">20x10</MenuItem>
+						<MenuItem eventKey="2">50x30</MenuItem>
+						<MenuItem eventKey="3">70x50</MenuItem>
+					</DropdownButton>
+				</ButtonToolbar>
+			</div>
+		);
 	}
 }
 
@@ -129,6 +173,15 @@ class Main extends React.Component {
 		return (
 			<div>
 				<h1>The Game of Life</h1>
+					<Buttons
+						playButton={this.playButton}
+						pauseButton={this.pauseButton}
+						slow={this.slow}
+						fast={this.fast}
+						clear={this.clear}
+						seed={this.seed}
+						gridSize={this.gridSize}
+					/>
 					<Grid 
 						gridFull={this.state.gridFull}
 						rows={this.rows}
