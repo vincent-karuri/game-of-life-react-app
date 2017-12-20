8480af2d248892b424b3cab11257c29a8809dccd
vkaruri <vkaruri@princeton.edu>
2017-12-19 20:41:03 -0500

    removed bugs in Box component and the grid renders correctly

--- gameoflife/src/index.js
+++ gameoflife/src/index.js
@@ -2,6 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class Box extends React.Component {
+    selectBox = () => {
+    	this.props.selectBox(this.props.row, this.props.col);
+    }
+
+	render() {
+		return (
+			<div
+				className={this.props.boxClass}
+				id={this.props.id}
+				onClick={this.selectBox}
+			/>
+		)
+	}
+}
+
 class Grid extends React.Component {
 	render() {
 		const width = this.props.cols * 14;
@@ -28,7 +44,7 @@ class Grid extends React.Component {
 
 		return (
 			<div className="grid" style={{width: width}}>
-				{{rowsArr}}
+				{rowsArr}
 			</div>
 		)
 	}
